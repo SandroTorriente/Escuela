5ab0da4ebe761124abc83670810953f105ae017a
SandroTorriente <146251059+SandroTorriente@users.noreply.github.com>
2023-10-01 20:58:47 -0700

    Update TareaJS_No1.js

--- Tarea1_JS_Sandro/TareaJS_No1.js
+++ Tarea1_JS_Sandro/TareaJS_No1.js
@@ -1,23 +1,41 @@
-let proyectos = [
-	{
-		"id": "0294618",
-		"nombre": "Vinland",
-		"ubicacion": "1era y 3era",
-		"presupuesto": 25000
-	},
-	{
-		"id": "1849502",
-		"nombre": "ProyectD",
-		"ubicacion": "Ave. 5ta y Los leones",
-		"presupuesto": 42000
-	},
-	{
-		"id": "6123027",
-		"nombre": "Hokai",
-		"ubicacion": "105 entre 120 y 6ta",
-		"presupuesto": 12000
+class Constructora {
+  constructor(name, proyectos, agregarptoyectos, actualizar, eliminarproyecto, rango){
+    this.name = name;
+    this.proyectos = proyectos;
+    this.agregarptoyectos = function(ProyectoConstruccion) {
+	 	proyectos.push(ProyectoConstruccion)};
+  this.eliminarproyecto = function(ProyectoConstruccion) {
+  for (let i = 0; i < proyectos.length; i++){
+    if (proyectos[i].id === ProyectoConstruccion.id) {
+      proyectos.splice(i, 1);
+    }
+  }
+};
+this.actualizar = function(ProyectoConstruccion) {
+	for (let i = 0; i < proyectos.length; i++){
+		if(proyectos[i].id === ProyectoConstruccion.id) {
+			proyectos[i].nombre = ProyectoConstruccion.nombre;
+			proyectos[i].ubicacion = ProyectoConstruccion.ubicacion;
+			proyectos[i].presupuesto = ProyectoConstruccion.presupuesto;
+			
+		}
+	}	
+};
+this.rango = function(minP, maxP) {
+	let proyectosfiltrados = []
+	for (let i = 0; i < proyectos.length; i++) {
+		let pres = proyectos[i].presupuesto;
+		if(pres >= minP && pres <= maxP){
+			proyectosfiltrados.push(proyectos[i]);
+		}
 	}
-]
+  proyectos = proyectosfiltrados;
+};
+
+  }
+
+
+}
 class ProyectoConstruccion {
 	constructor(id, nombre, ubicacion, presupuesto){
 		this.id = id;
@@ -27,38 +45,15 @@ class ProyectoConstruccion {
 	}
 }
 
- function crearptoyectos(id, nombre, ubicacion, presupuesto) {
-	let PC = new PoyecProyectoConstruccion(id, nombre, ubicacion, presupuesto)
-	proyectos.push(PC)
-}
 
-function eliminarproyecto(proyectos, id) {
-  for (let i = 0; i < proyectos.length; i++) {
-    if (proyectos[i].id === id) {
-      proyectos.splice(i, 1);
-    }
-  }
-}
 
-function actualizar (id, nombre, ubicacion, presupuesto) {
-	for (let i = 0; i < proyectos.length; i++){
-		if(proyectos[i].id === id) {
-			proyectos[i].nombre = nombre;
-			proyectos[i].ubicacion = ubicacion;
-			proyectos[i].presupuesto = presupuesto;
-			
-		}
-	}
-	
-}
 
-function rango (minP, maxP) {
-	let proyectosfiltrados = []
-	for (let i = 0; i < proyectos.length; i++) {
-		let pres = proyectos[i].presupuesto;
-		if(pres >= minP && pres <= maxP){
-			proyectosfiltrados.push(proyectos[i]);
-		}
-	}
-	console.log(proyectosfiltrados);
-}
+
+
+
+const PC1 = new ProyectoConstruccion('1234567', 'Vinland', 'Habana Vieja', 10000);
+
+const constructora = new Constructora('Beimax', proyectos);
+
+constructora.agregarptoyectos(PC1);
+
